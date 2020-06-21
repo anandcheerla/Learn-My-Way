73141fda51780990475a9cd716946ab2dd0eea79
anandcheerla <anandcheerla999@gmail.com>
2020-06-21 20:41:25 +0530

    added comments

--- frontend/src/views/Articles.js
+++ frontend/src/views/Articles.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
+
+//user defined packages or files
 import '.././App.css';
 import Article from './Article.js';
 // import Unit from './Unit.js';
@@ -15,106 +17,103 @@ class Articles extends React.Component{
       showArticleCreationForm: false,
       showCreateArticleButton: true
     };
-  }
+  }//constructor end
 
   componentDidMount(){
     console.log("articles component mounted");
   }
 
 
-createArticle=(event)=>{
-    event.preventDefault();
-    let formData={
-      heading: event.target.heading.value,
-      description: event.target.description.value
-    }
-    axios.post("/new-article",formData).then(res=>{
-        let newlyCreatedArticle={...res.data};
-        if(res.data._id){
-          this.setState({
-            articles: [...this.state.articles,newlyCreatedArticle],
-            showArticleCreationForm: false,
-            showCreateArticleButton: true
-          });
+  //create article api call to /new-article route and append newly created articles to articles property of state
+  createArticle=(event)=>{
+      event.preventDefault();
+      let formData={
+        heading: event.target.heading.value,
+        description: event.target.description.value
+      }
+      axios.post("/new-article",formData).then(res=>{
+          let newlyCreatedArticle={...res.data};
+          if(res.data._id){
+            this.setState({
+              articles: [...this.state.articles,newlyCreatedArticle],
+              showArticleCreationForm: false,
+              showCreateArticleButton: true
+            });
+          }
+      });
+  }//createArticle method end
 
-        }
-    });
-}
-
-cancelButtonClickHandler=()=>{
-  this.setState({showArticleCreationForm:false,showCreateArticleButton:true});
-}
-articleCreationForm=()=>{
-
-    return (
-      <form name="createArticle" onSubmit={this.createArticle}>
-          <div className="form-group">
-            <label htmlFor="heading">Name</label>
-            <input type="text" className="form-control" id="articleHeading" name="heading" placeholder="heading"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea type="textarea" className="form-control" id="description" name="description" placeholder="description"/>
-          </div>
-          <button type="submit" className="btn btn-primary">Create</button>
-          <span> <button onClick={this.cancelButtonClickHandler} className="btn btn-primary">Cancel</button></span>
-      </form>
-
-      );
-
-  }
-
-  createArticleButtonClickHandler=()=>{
-
-      this.setState({showArticleCreationForm:true,showCreateArticleButton:false});
-
-  }
+  cancelButtonClickHandler=()=>{
+    this.setState({showArticleCreationForm:false,showCreateArticleButton:true});
+  }//cancelButtonClickHandler method end
 
 
-  render(){
+  //this method is to create and return the article creation form
+  articleCreationForm=()=>{
+      return (
+        <form name="createArticle" onSubmit={this.createArticle}>
+            <div className="form-group">
+              <label htmlFor="heading">Name</label>
+              <input type="text" className="form-control" id="articleHeading" name="heading" placeholder="heading"/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea type="textarea" className="form-control" id="description" name="description" placeholder="description"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Create</button>
+            <span> <button onClick={this.cancelButtonClickHandler} className="btn btn-primary">Cancel</button></span>
+        </form>
+        );
+    }//articleCreationForm end
 
-    let reverseDirectionForArticlesArray={
-          "display": "flex",
-          "flex-direction": "column-reverse"
-    };
 
-    let articleStyle={
-      "margin-bottom": "15px",
-      
-    }
+    createArticleButtonClickHandler=()=>{
+        this.setState({showArticleCreationForm:true,showCreateArticleButton:false});
+    }//createArticleButtonClickHandler method end
 
-    let newArticleButtonStyle={
-        "position":"fixed",
-        "top":"30",
-        "right":"0"
-    }
-    return (
-       <div>
+
+    render(){
+      //inline properties or variables
+      let reverseDirectionForArticlesArray={
+            "display": "flex",
+            "flex-direction": "column-reverse"
+      };
+
+      let articleStyle={
+        "margin-bottom": "15px", 
+      }
+      let newArticleButtonStyle={
+          "position":"fixed",
+          "top":"30",
+          "right":"0"
+      }
+
+      return (
+        <div>
           <div>
             {
               !this.state.showArticleCreationForm ?
               <h2>My Articles</h2> : <h2>Create Article</h2>
             }
             {
-
               this.state.showCreateArticleButton ?
               <button style={newArticleButtonStyle} onClick={this.createArticleButtonClickHandler} className="btn btn-outline-primary">New Article</button>
               :
               null
             }
           </div>
-         {this.state.showArticleCreationForm ? this.articleCreationForm() : null}
+          {this.state.showArticleCreationForm ? this.articleCreationForm() : null}
           <ul className="list-group" style={reverseDirectionForArticlesArray}>
-             {
-              !this.state.showArticleCreationForm &&
-              this.state.articles.map(element=>(
-                <li className="list-group-item" style={articleStyle}><Article dbId={element._id} heading={element.heading} description={element.description} units={element.units}></Article></li>
-              ))
-             }
+           {
+            !this.state.showArticleCreationForm &&
+            this.state.articles.map(element=>(
+              <li className="list-group-item" style={articleStyle}><Article dbId={element._id} heading={element.heading} description={element.description} units={element.units}></Article></li>
+            ))
+           }
           </ul>
-       </div>
-    );
-  }
-}
+        </div>
+      );
+    }//render method end
+}//class Articles end
 
 export default Articles;
