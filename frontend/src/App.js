cc6e614bd10f305ce2644e561c65b4c702ba65cb
anandcheerla <anandcheerla999@gmail.com>
2020-06-22 19:17:17 +0530

    added local storage for persistence of state and added logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import ls from 'local-storage';
 
 //user defined packages or files
 import Articles from './views/Articles.js';
@@ -12,12 +13,12 @@ class Akc extends React.Component{
       super(props);
       //initial state
       this.state={
-        articles:[],
-        showRegisterForm: false,
-        showLoginForm: false,
-        loginSuccess: false,
-        fetchedArticlesFromDb: false
-        
+        articles: ls.get("articles") || [],
+        showRegisterForm: ls.get("showRegisterForm") || false,
+        showLoginForm: ls.get("showLoginForm") || false,
+        loginSuccess: ls.get("loginSuccess") || false,
+        fetchedArticlesFromDb: ls.get("fetchedArticlesFromDb") || false
+
       };
     }//constructor end
 
@@ -25,17 +26,21 @@ class Akc extends React.Component{
     fetchArticlesFromDb=()=>{
       axios.get("/articles").then(
           res=>{
+            let articles_temp_var=[...res.data];
+            let fetchArticlesFromDb_temp_var=true;
             this.setState({
-              articles: [...res.data],
-              fetchedArticlesFromDb: true
+              articles: articles_temp_var,
+              fetchedArticlesFromDb: fetchArticlesFromDb_temp_var
             });
+            ls.set("articles",articles_temp_var);
+            ls.set("fetchedArticlesFromDb",fetchArticlesFromDb_temp_var);
           }
       )
     }//fetchArticlesFromDb method end
 
 
     componentDidMount(){
-      
+      // ls.clear();
     }
    
 
@@ -48,12 +53,20 @@ class Akc extends React.Component{
       }
 
       axios.post("/login",formData).then((res)=>{
-        if(res.data=="success"){  
+        if(res.data=="success"){ 
+          let loginSuccess_temp_var=true;
+          let showRegisterForm_temp_var=false;
+          let showLoginForm_temp_var=false; 
+
           this.setState({
-              loginSuccess:true,
-              showRegisterForm:false,
-              showLoginForm:false
+              loginSuccess:loginSuccess_temp_var,
+              showRegisterForm:showRegisterForm_temp_var,
+              showLoginForm:showLoginForm_temp_var
           });
+
+          ls.set("loginSuccess",loginSuccess_temp_var);
+          ls.set("showRegisterForm",showRegisterForm_temp_var);
+          ls.set("showLoginForm",showLoginForm_temp_var);
         }
         else{
           alert("Incorrect credentials");
@@ -62,6 +75,26 @@ class Akc extends React.Component{
 
     }//userLogin method end
 
+
+
+    userLogout=(event)=>{
+
+      // console.log("event akc is "+event);
+      event.preventDefault();
+      axios.get("/logout").then((res)=>{
+        console.log("logout successful");
+      });
+      ls.clear();
+      this.setState({
+        articles: [],
+        showRegisterForm: false,
+        showLoginForm: false,
+        loginSuccess: false,
+        fetchedArticlesFromDb: false
+
+      });
+
+    }
 
     //registration api call to /register with the necessary details
     userRegistration=(event)=>{
@@ -76,10 +109,16 @@ class Akc extends React.Component{
       }
 
       axios.post("/register",formData).then((res)=>{
+
+        let showRegisterForm_temp_var=false;
+        let showLoginForm_temp_var=true;
         this.setState({
-          showRegisterForm:false,
-          showLoginForm:true
+          showRegisterForm:showRegisterForm_temp_var,
+          showLoginForm:showLoginForm_temp_var
         });
+
+        ls.set("showRegisterForm",showRegisterForm_temp_var);
+        ls.set("showLoginForm",showLoginForm_temp_var);
       });
     }//userRegistration method end
 
@@ -137,6 +176,8 @@ class Akc extends React.Component{
     }//userLoginForm method end
 
 
+
+
     myArticles=()=>{
       return (
          <div>
@@ -154,18 +195,22 @@ class Akc extends React.Component{
               {
                 !this.state.loginSuccess &&
                   <div> 
-                    <button onClick={()=>this.setState({showLoginForm: true,showRegisterForm: false})} className="btn btn-primary">Login</button>
+                    <button onClick={()=>{this.setState({showLoginForm: true,showRegisterForm: false});ls.set("showLoginForm",true);ls.set("showRegisterForm",false);}} className="btn btn-primary">Login</button>
                   </div>
                }
                {
                  !this.state.loginSuccess &&
                   <div> 
-                    <button onClick={()=>this.setState({showRegisterForm: true,showLoginForm:false})} className="btn btn-primary">Register</button>
+                    <button onClick={()=>{this.setState({showRegisterForm: true,showLoginForm:false});ls.set("showRegisterForm",true);ls.set("showLoginForm",false);}} className="btn btn-primary">Register</button>
                   </div>
                }
                 <div> 
                   <button onClick={()=>this.fetchArticlesFromDb()} className="btn btn-primary">My Articles</button>
                 </div>
+                 {
+                    this.state.loginSuccess && 
+                    <button onClick={(event)=>this.userLogout(event)} className="btn btn-primary">Logout</button>
+                 }
               </nav>
             </div>
 
@@ -176,6 +221,8 @@ class Akc extends React.Component{
               {this.state.showRegisterForm? this.userRegistrationForm():null}
 
               {this.state.loginSuccess && this.state.fetchedArticlesFromDb ?this.myArticles():null}
+              
+              
             </div>
           </div>
       );
