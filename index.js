299f0cc1cd0470663236baeee9a274087a341baf
anandcheerla <anandcheerla999@gmail.com>
2020-07-25 19:02:49 +0530

    changed line which takes port number

--- index.js
+++ index.js
@@ -382,7 +382,7 @@ app.delete("/unit-delete/:articleId/:unitId",isLoggedIn,function(req,res){
 
 //--------------------------------------------------------
 
-const PORT= 5000 || process.env.PORT;
+const PORT= process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
 	console.log("listening to the "+PORT);
