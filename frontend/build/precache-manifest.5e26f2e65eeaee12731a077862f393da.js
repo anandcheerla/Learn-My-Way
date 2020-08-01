c69650ddb444059541713601a1744ed35e5c6f6a
anandcheerla <anandcheerla999@gmail.com>
2020-08-01 18:36:13 +0530

    build done

---
+++ frontend/build/precache-manifest.5e26f2e65eeaee12731a077862f393da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26b2dad620cbdd91b1840b2f5717ccf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2cd99aaac3062f8ac81",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "339520d7f65d487cb4e3",
+    "url": "./static/css/main.2a9af577.chunk.css"
+  },
+  {
+    "revision": "b2cd99aaac3062f8ac81",
+    "url": "./static/js/2.aab9421a.chunk.js"
+  },
+  {
+    "revision": "86493497955f2c03ad4534f44ae20baa",
+    "url": "./static/js/2.aab9421a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339520d7f65d487cb4e3",
+    "url": "./static/js/main.c9183027.chunk.js"
+  },
+  {
+    "revision": "e736dabb8ee024a7b980",
+    "url": "./static/js/runtime-main.3993e964.js"
+  }
+]);
