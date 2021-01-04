ff745ffc7291b55516b72d640d21f780923882b0
anandcheerla <anandcheerla999@gmail.com>
2021-01-04 19:54:38 +0530

    build related files

---
+++ frontend/build/precache-manifest.71651a1aa0f109f1552f558aca22a4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbbf735b4a68400489fce3b076fadfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b7a0cfecd0dacd020d",
+    "url": "/static/css/main.72e64428.chunk.css"
+  },
+  {
+    "revision": "fac8a6e33e6b1b80c672",
+    "url": "/static/js/2.a8d93462.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.a8d93462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54b7a0cfecd0dacd020d",
+    "url": "/static/js/main.d147737a.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
