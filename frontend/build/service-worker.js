1b49a5c85b780641ec7c991d82f8383994432a99
anandcheerla <anandcheerla999@gmail.com>
2021-01-04 12:24:22 +0530

    build related files

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dccb9218a362eb15ec7648d87401933.js"
+  "/precache-manifest.86661df454a45e1c6e9096ad02921004.js"
 );
 
 self.addEventListener('message', (event) => {
