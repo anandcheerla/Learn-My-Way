757807a3871ccfacb19e4483102f0e482ba8779d
anandcheerla <anandcheerla999@gmail.com>
2021-01-04 12:11:00 +0530

    fixing prod issue

---
+++ frontend/build/precache-manifest.86661df454a45e1c6e9096ad02921004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4799822bbcd180f6be38c03467e4b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7143f2bc92683bc60044",
+    "url": "/static/css/main.72e64428.chunk.css"
+  },
+  {
+    "revision": "0b6059b1a312eccf94eb",
+    "url": "/static/js/2.c7ec6cfd.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.c7ec6cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7143f2bc92683bc60044",
+    "url": "/static/js/main.94d5ec8c.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
