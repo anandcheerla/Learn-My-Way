5b278cb0f42d059159bc762c0c5e3e479ada1467
anandcheerla <anandcheerla999@gmail.com>
2021-05-22 13:24:09 +0530

    config changes

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
-import dotenv from 'dotenv'
 
-dotenv.config();
+//process.env.PROD_DB_URI works because this was added in heroku config vars in heroku
 
 const db_uri = process.env.PORT ? process.env.PROD_DB_URI : process.env.DEV_DB_URI;
 
